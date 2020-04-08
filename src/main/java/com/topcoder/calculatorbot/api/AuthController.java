e86969cf14b93557e37d82b81f92ee76b62d394b
Chris Brumfitt <cbrumfit@jaguarlandrover.com>
2020-04-08 22:53:01 +0100

    Returned Object from AuthController to see all properties from the response

--- src/main/java/com/topcoder/calculatorbot/api/AuthController.java
+++ src/main/java/com/topcoder/calculatorbot/api/AuthController.java
@@ -51,7 +51,8 @@ public class AuthController {
 
     HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(params, headers);
 
-    ResponseEntity<SlackAuthResponse> res = restTemplate.postForEntity(BASE_URL, request, SlackAuthResponse.class);
+//    ResponseEntity<SlackAuthResponse> res = restTemplate.postForEntity(BASE_URL, request, SlackAuthResponse.class);
+    ResponseEntity<Object> res = restTemplate.postForEntity(BASE_URL, request, Object.class);
     return res;
   }
 
