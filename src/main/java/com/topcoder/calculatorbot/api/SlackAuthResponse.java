2cc505c077bd40bbe632e70599a6d2a2ab5a7608
Chris Brumfitt <cbrumfit@jaguarlandrover.com>
2020-04-08 22:42:28 +0100

    Added bot to the auth response

--- src/main/java/com/topcoder/calculatorbot/api/SlackAuthResponse.java
+++ src/main/java/com/topcoder/calculatorbot/api/SlackAuthResponse.java
@@ -1,11 +1,14 @@
 package com.topcoder.calculatorbot.api;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.topcoder.calculatorbot.api.model.BotResponse;
 
 public class SlackAuthResponse {
 
   @JsonProperty("access_token")
   private String accessToken;
+
+  private BotResponse bot;
 
   public String getAccessToken() {
     return accessToken;
